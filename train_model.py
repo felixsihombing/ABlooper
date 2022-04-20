32f1fee19c441a62ef27f009a37fa15f18b6d8cd
Fabian Spoendlin <f.spoendlin@bluewin.ch>
2022-04-20 14:02:00 +0200

    train_model.py changed to train on filtered data, batch size 4, radam

--- train_model.py
+++ train_model.py
@@ -10,6 +10,7 @@ from retrain_ablooper import *
 
 
 
+print('Import data')
 # import data
 with open('train_data/CDR_BB_coords.npy', 'rb') as infile:
     CDR_BB_coords = np.load(infile, allow_pickle=True)
@@ -23,11 +24,27 @@ with open('train_data/CDR_BB_coords_test.npy', 'rb') as infile:
 with open('train_data/CDR_seqs_test.npy', 'rb') as infile:
     CDR_seqs_test = np.load(infile, allow_pickle=True)
 
+print('Number of fabs after import', len(CDR_seqs))
 
+
+
+print('Filter data')
+# filter input data
+CDR_seqs, CDR_BB_coords = filter_CDR_length(CDR_seqs, CDR_BB_coords, length_cutoff=22)
+CDR_seqs_test, CDR_BB_coords_test = filter_CDR_length(CDR_seqs_test, CDR_BB_coords_test, length_cutoff=22)
+
+CDR_seqs, CDR_BB_coords = filter_CA_distance(CDR_seqs, CDR_BB_coords)
+CDR_seqs_test, CDR_BB_coords_test = filter_CA_distance(CDR_seqs_test, CDR_BB_coords_test)
+
+CDR_seqs, CDR_BB_coords = remove_test_set_identities(CDR_seqs, CDR_BB_coords, CDR_seqs_test)
+
+
+
+
+print('Format data')
 # torch settings
 device = "cuda" if torch.cuda.is_available() else "cpu"
 torch.set_default_dtype(torch.float)
-
 
 # prepare model inputs/outputs
 geomins, node_encodings = prepare_model_inputs(CDR_seqs, CDR_BB_coords)
@@ -48,17 +65,18 @@ geomouts_test = pad_list_of_tensors(geomouts_test)
 
 data = concatenate_data(node_encodings, geomins, geomouts, masks)
 test = concatenate_data(node_encodings_test, geomins_test, geomouts_test, masks_test)
-len(data), len(test)
-
-print('Data formated')
+print('Size train/val set: ', len(data), ', size test set:', len(test))
 
 
+
+
+print('Prepare dataloaders')
 # split in train and validation sets
 train, validation = train_test_split(data, test_size=100, random_state=42)
 
 print(f'Size train set: {len(train)}, val set: {len(validation)}, test set: {len(test)}')
 
-batch_size = 1
+batch_size = 4
 num_workers = 1
 train_dataloader = torch.utils.data.DataLoader(train, 
                                                batch_size=batch_size,   # Batch size
@@ -82,14 +100,15 @@ test_dataloader = torch.utils.data.DataLoader(test,
                                               )
 
 
-# train model
-print('Start training')
 
+
+print('Start training')
+# train model
 # initialise model
 model = MaskDecoyGen(decoys=1).to(device = device).float()
 
 # set optimiser
-optimiser = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-3)
+optimiser = torch.optim.RAdam(model.parameters(), lr=1e-3, weight_decay=1e-3)
 
 # Step to actually train the network
-train_losses, val_losses = train_model(model, optimiser, train_dataloader, val_dataloader, training_name='-1804-Adam-1' ,n_epochs=5000, patience=150, decoys=1)
+train_losses, val_losses = train_model(model, optimiser, train_dataloader, val_dataloader, training_name='-2004-RAdam-4' , n_epochs=5000, patience=150, decoys=1)
